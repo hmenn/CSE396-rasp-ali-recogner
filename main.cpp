5f8bcec207a430f5ce3bd5469920f20c709c2dd2
Hasan MEN <hmen.56@gmail.com>
2017-03-24 12:58:14 +0300

    opencv support added

--- main.cpp
+++ main.cpp
@@ -1,6 +1,39 @@
+#include "opencv2/imgcodecs.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+using namespace cv;
+using namespace std;
+
+static void help() {
+    cout << "\nThis program demonstrates circle finding with the Hough transform.\n"
+            "Usage:\n"
+            "./houghcircles <image_name>, Default is ../data/board.jpg\n" << endl;
+}
+
+int main(int argc, char **argv) {
+    cv::CommandLineParser parser(argc, argv,
+                                 "{help h ||}{@image|../data/board.jpg|}"
+    );
+    if (parser.has("help")) {
+        help();
+        return 0;
+    }
+    string filename = parser.get<string>("@image");
+    if (filename.empty()) {
+        help();
+        cout << "no image_name provided" << endl;
+        return -1;
+    }
+    Mat img = imread(filename, 0);
+    if (img.empty()) {
+        help();
+        cout << "can not open " << filename << endl;
+        return -1;
+    }
+
+    imshow("detected circles", img);
+    waitKey();
     return 0;
 }
